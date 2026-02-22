f06e00630728a068e9f401f3a2ce88684a1d9512
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-02-22 20:11:53 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -1,14 +1,13 @@
 const tg = window.Telegram.WebApp;
 let count = 0;
-const queryId = tg.initDataUnsafe.chat.id
 
 document.getElementById('add').onclick = () => {
     count++;
-    document.getElementById('display').innerText = queryId;
+    document.getElementById('display').innerText = count;
 };
 
 document.getElementById('send').onclick = async () => {
-    const queryId = tg.initDataUnsafe.chat.id; // Важно для Inline
+    const queryId = tg.initDataUnsafe.user.first_name;
     const dataToSend = { query_id: String(queryId), count: Number(count) };
     const response = await fetch('http://127.0.0.1:8000/send-text', {
         method: 'POST',
