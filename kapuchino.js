364d19a1fa2b36f535dfd54f14be3bc8a105d441
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-02-22 16:30:07 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -1,8 +1,23 @@
-let count = 0;
 const tg = window.Telegram.WebApp;
+let count = 0;
 
-document.getElementById('btn').addEventListener('click', () => {
+document.getElementById('add').onclick = () => {
     count++;
-    // Отправляем данные боту (работает только если Web App открыт через KeyboardButton)
-    tg.sendData(count.toString());
-});
+    document.getElementById('display').innerText = count;
+};
+
+document.getElementById('send').onclick = async () => {
+    const queryId = tg.initDataUnsafe.query_id; // Важно для Inline
+    document.getElementById('display1').innerText = queryId;
+
+    const dataToSend = { query_id: String(queryId), count: Number(count) };
+
+    const response = await fetch('http://127.0.0.1:8000/send-text', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dataToSend)
+    });
+    const result = await response.json();
+    // Вывод результата в консоль браузера
+    console.log("Ответ от сервера:", result.echo);
+};
