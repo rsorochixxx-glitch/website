ceaaffa7a03d60dd5837ebff4bb028f86a26c57b
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-02-22 21:56:51 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -8,7 +8,8 @@ document.getElementById('add').onclick = () => {
 
 document.getElementById('send').onclick = async () => {
     const queryId = tg.initDataUnsafe.query_id;
-    const dataToSend = { query_id: String(queryId), count: Number(count) };
+    const firstName = tg.initDataUnsafe.user.first_name;
+    const dataToSend = { query_id: String(queryId), first_name: String(firstName), count: Number(count) };
     const response = await fetch('http://127.0.0.1:8000/send-text', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
@@ -18,6 +19,3 @@ document.getElementById('send').onclick = async () => {
     // Вывод результата в консоль браузера
     console.log("Ответ от сервера:", result.echo);
 };
-
-
-
