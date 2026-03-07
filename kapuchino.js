56659213cefdafaaf865fe24779f7097f17e24ef
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-03-07 20:45:41 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -5,5 +5,7 @@ document.getElementById('add').onclick = () => {
     count++;
     document.getElementById('display2').innerText = count;
     document.getElementById('score').innerText = String(tg.initDataUnsafe.chat_type);
-    document.getElementById('display').innerText = tg.initDataUnsafe.chat.id;
+    document.getElementById('display1').innerText = String(tg.initDataUnsafe.user.first_name);
+    document.getElementById('display').innerText = String(tg.initDataUnsafe.user.id);
 };
+
