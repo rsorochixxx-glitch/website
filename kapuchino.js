507296fa889e91a867b37b2b4330e8cef23638fc
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-03-07 22:42:02 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -1,12 +1,20 @@
-const tg = window.Telegram.WebApp;
 let count = 0;
-
 document.getElementById('add').onclick = () => {
     count++;
-    console.log(tg.initDataUnsafe)
-    document.getElementById('display2').innerText = count;
-    document.getElementById('score').innerText = String(tg.initDataUnsafe.chat_type);
-    document.getElementById('display1').innerText = String(tg.initDataUnsafe.chat.type);
-    document.getElementById('display').innerText = String(tg.initDataUnsafe.chat.id);
+    console.log()
 };
 
+function greet(count, gameName) {
+    const tg = window.Telegram.WebApp;
+    const userId = String(tg.initDataUnsafe.user.id);
+    const firstName = String(tg.initDataUnsafe.user.first_name);
+    const dataToSend = { user_id: userId, first_name: firstName, count: count, game: gameName };
+    const response = await fetch('https://back-roman9128.amvera.io/send-text', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(dataToSend)
+    });
+    const result = await response.json();
+    document.getElementById('score').innerText = result.echo;    
+}
+tg.onEvent('close', greet(count, 'Clicker'));
