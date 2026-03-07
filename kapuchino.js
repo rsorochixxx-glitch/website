3d21206ae318bc9d5e6f29ed5c03959492947e4c
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-03-07 21:13:44 +0300

    Update kapuchino.js

--- kapuchino.js
+++ kapuchino.js
@@ -3,9 +3,10 @@ let count = 0;
 
 document.getElementById('add').onclick = () => {
     count++;
-    console.log(tg.initData)
+    console.log(tg.initDataUnsafe)
     document.getElementById('display2').innerText = count;
     document.getElementById('score').innerText = String(tg.initDataUnsafe.chat_type);
     document.getElementById('display1').innerText = String(tg.initDataUnsafe.chat.type);
     document.getElementById('display').innerText = String(tg.initDataUnsafe.chat.id);
 };
+
