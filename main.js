e9ab33e4c9886bcfd50b36ff33f751fb21f04f89
rsorochixxx-glitch <rsorochixxx@gmail.com>
2026-03-14 23:56:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,11 @@ console.log("Получен параметр:", chat_id);
 tg.ready();
 
 document.getElementById('btnClicker').onclick = function() {
-    location.href = 'square/index.html';
+    location.href = `square/index.html?chat_id=${chat_id}`;
+};
+
+document.getElementById('btnSnake').onclick = function() {
+    location.href = `snake/snake.html?chat_id=${chat_id}`;
 };
 
 async function greet(count, gameName) {
